eb554474ecbbc243d69bd3d394beb6e0b13a451f
Kyle Falconer <kfalconer@gmail.com>
2014-06-03 01:08:05 -0500

    added some helper methods for blocking pages (see http://stackoverflow.com/a/4983354/940217 )

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def hide_from_unauthenticated
+    not_found unless user_signed_in?
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+  
 end
